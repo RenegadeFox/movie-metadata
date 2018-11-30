cd7f7b60f31823de845d76fc3914fb75a3f2cd13
Martin <martincox99@me.com>
2018-11-30 16:48:03 +0300

    Refactored metadata.js to comply with node-style-guide

--- dev/programmatic.js
+++ dev/programmatic.js
@@ -1,92 +1,25 @@
-const { getMetadata } = require('../src/')
+const { getMetadata } = require('../src/');
 
 
 /**
  * Available Options (with their defaults)
  */
 const options = {
-  /**
-   * Where to save the JSON file, with the fetched metadata, to.
-   * "%source%" is a placeholder for the source file.
-   *
-   * If set to false the fetched metadata will be returned as a
-   * Promise Object.
-   * ===========================================================================
-   * @default '%source%-metadata.json'
-   * @type    {String|Boolean}
-   */
-  destination: false,
+  dest     : false,
+  notfound : false,
+  key      : '[YOUR_API_KEY]',
+  verbose  : true,
+  source   : [
+    'Man\'s Chest',
+    'Ocean\'s Eight'
+  ]
+};
 
-  /**
-   * Path where to save the movies that are not found on the omdb API server
-   * "%source%" is a placeholder for the source file (minus the .json extension)
-   *
-   * If set to false the not found movies will be returned as a
-   * Promise Array of movie titles.
-   * ===========================================================================
-   * @default '%source%-notfound.json'
-   * @type    {String|Boolean}
-   */
-  notfound: false,
-
-  /**
-   * Whether or not to show each movie as its metadata is being fetched.
-   * Enabling this will disable the {progress} option.
-   *
-   * @default false
-   * @type    {Boolean}
-   */
-  verbose: false,
-
-  /**
-   * Whether or not to show a progress bar while the movie metadata
-   * is being fetched. Enabling this will disable the {verbose} option.
-   *
-   * @default true
-   * @type    {Boolean}
-   */
-  progress: true,
-
-  /**
-   * Whether or not to overwrite the {source} JSON file with the updated
-   * JSON metadata.
-   *
-   * If the "destination" or "notfound" option is set to false, this is
-   * automatically disabled, and the metadata is returned as a Promise instead.
-   *
-   * @default false
-   * @type    {Boolean}
-   */
-  overwrite: false,
-
-  /**
-   * Amount of time to wait before aborting the request for metadata from the
-   * API server. Will automatically continue, and the movie metadata will
-   * not be skipped.
-   *
-   * @default 120000 (2 minutes)
-   * @type    {Number}
-   */
-  timeout: 120000,
-
-  /**
-   * omdb API key. Your omdb API key goes here
-   *
-   * @default {Required}
-   * @type    {String}
-   */
-  key: '[YOUR_OMDB_API_KEY]',
-
-  /**
-   * Where to get the movie titles to search for
-   *
-   * @default {Required}
-   * @type    {Array}
-   */
-  source: ['dead man\'s chest', 'at world\'s end', 'ralph breaks the internet', 'Ocean\'s Eight']
-}
 getMetadata(options)
-  .then(data => {
-    console.log(data)
+  .then(({ fetched, notFound }) => {
+    console.log(fetched.map(data => {
+      return { title: data.Title, year: data.Year }
+    }), notFound);
+
   })
-  .catch(err => console.error(err))
+  .catch(err => console.error(err));
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = {
   // TODO: Add a getPoster method to fetch movie posters
   getMetadata: require('./metadata')
-}
+};
